c477f5d20cfecd7388978c0d9e4d537b25670d71
wang051211 <602025574@qq.com>
2015-07-17 16:53:34 +0800

    identity card

--- IdentityCard.java
+++ IdentityCard.java
@@ -1,107 +1,110 @@
 package java1;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class IdentityCard {
 
-	public static int isIdentityCard(ArrayList<Integer> str) {
+	public static int isIdentityCard(ArrayList<String> str) {
+		ArrayList<Integer> a = new ArrayList<Integer>();
+		Iterator iter = str.iterator();
+		while(iter.hasNext()){
+			a.add(Integer.parseInt((String) iter.next()));
+		}
 		int sum = 0;
-		sum = str.get(0) * 7 + str.get(1) * 9 + str.get(2) * 10 + str.get(3)
-				* 5 + str.get(4) * 8 + str.get(5) * 4 + str.get(6) * 2
-				+ str.get(7) * 1 + str.get(8) * 6 + str.get(9) * 3
-				+ str.get(10) * 7 + str.get(11) * 9 + str.get(12) * 10
-				+ str.get(13) * 5 + str.get(14) * 8 + str.get(15) * 4
-				+ str.get(16) * 2;
+		sum = a.get(0)* 7 +a.get(1)* 9 + a.get(2) * 10 + a.get(3)
+				* 5 + a.get(4)* 8 + a.get(5)* 4 + a.get(6)* 2
+				+ a.get(7)* 1 + a.get(8)* 6 + a.get(9)* 3
+				+ a.get(10) * 7 + a.get(11) * 9 + a.get(12) * 10
+				+ a.get(13) * 5 + a.get(14) * 8 + a.get(15) * 4
+				+ a.get(16) * 2;
 		int k = sum % 11;
 		return k;
 
 	}
 
 	public static void main(String[] args) {
-		ArrayList<Integer> ai = new ArrayList<Integer>();
+		ArrayList<String> ai = new ArrayList<String>();
 		System.out.print("请输入18位身份证号码：");
 		Scanner s = new Scanner(System.in);
 		String str = s.next();
-		String[] number = str.split(",");
-		if ((number.length) != 18) { 
-			System.out.println();
-		    System.out.print("输入有误！请重新输入："); 
-		    str = s.nextLine();
-	    }
-		 
-		for (int i = 0; i < number.length; i++) {
-			ai.add(Integer.parseInt(number[i]));
+		//String[] number = str.split(",");
+		char[] chars=str.toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			ai.add((chars[i]+""));
 		}
-		System.out.println(isIdentityCard(ai));
 		switch (isIdentityCard(ai)) {
 		case 0:
-			if ("1".endsWith(number[17]))
+			if (str.charAt(17)=='1') {
 				System.out.println("身份证合法！");
+			}
 			else
 				System.out.println("身份证不合法！");
 			break;
-
 		case 1:
-			if ("0".endsWith(number[17]))
+			if (str.charAt(17)=='0')
 				System.out.println("身份证合法！");
 			else
 				System.out.println("身份证不合法！");
 			break;
 		case 2:
-			if ("x".endsWith(number[17]))
+			if (str.charAt(17)=='x')
 				System.out.println("身份证合法！");
 			else
 				System.out.println("身份证不合法！");
 			break;
 
 		case 3:
-			if ("9".endsWith(number[17]))
+			if (str.charAt(17)=='9'){
 				System.out.println("身份证合法！");
+			}	
 			else
 				System.out.println("身份证不合法！");
 			break;
 		case 4:
-			if ("8".endsWith(number[17]))
+			if (str.charAt(17)=='8')
 				System.out.println("身份证合法！");
 			else
 				System.out.println("身份证不合法！");
 			break;
 		case 5:
-			if ("7".endsWith(number[17]))
+			if (str.charAt(17)=='7')
 				System.out.println("身份证合法！");
 			else
 				System.out.println("身份证不合法！");
 			break;
 		case 6:
-			if ("6".endsWith(number[17]))
+			if (str.charAt(17)=='6')
 				System.out.println("身份证合法！");
 			else
 				System.out.println("身份证不合法！");
 			break;
 		case 7:
-			if ("5".endsWith(number[17]))
+			if (str.charAt(17)=='5')
 				System.out.println("身份证合法！");
 			else
 				System.out.println("身份证不合法！");
 			break;
 		case 8:
-			if ("4".endsWith(number[17]))
+			if (str.charAt(17)=='4')
 				System.out.println("身份证合法！");
 			else
 				System.out.println("身份证不合法！");
 			break;
 		case 9:
-			if ("3".endsWith(number[17]))
+			if (str.charAt(17)=='3')
 				System.out.println("身份证合法！");
 			else
 				System.out.println("身份证不合法！");
 			break;
 		case 10:
-			if ("2".endsWith(number[17]))
+			if (str.charAt(17)=='2')
 				System.out.println("身份证合法！");
 			else
 				System.out.println("身份证不合法！");
+			break;
+		default:
 			break;
 		}
 	}
